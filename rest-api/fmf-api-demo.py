ce38a90eeaffe7b8cfa5f9ffa7f10035c8b6aa9a
Jason Costomiris <jcostom@jasons.org>
2021-03-31 16:02:33 -0400

    Added REST API demo

---
+++ rest-api/fmf-api-demo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import requests
+from xml.etree import ElementTree as ET
+
+USER="autobot"
+PWD="juniper123"
+DEVICES = ["shire.jasons.org"]
+
+FILTER = """
+<get-config>
+    <source>
+        <running/>
+    </source>
+    <filter type="subtree">
+        <configuration>
+            <system>
+                <login/>
+            </system>
+        </configuration>
+    </filter>
+</get-config>
+"""
+
+for device in DEVICES:
+    reply = requests.post("http://{}:3001/rpc".format(device),data=FILTER,
+        auth=requests.auth.HTTPBasicAuth(USER, PWD),
+        headers={"Accept": "application/xml","Content-Type": "application/xml"})
+
+    XML_lines = "\n".join(reply.text.splitlines()[3:-1])
+    root = ET.fromstring(XML_lines)
+
+    print("\nDevice: {} has the following usernames in its local DB:"
+          .format(device))
+    names = root.findall('.//user')
+    for name in names:
+        username = name.find('name').text
+        print(username)
